c91d6578c3b47d8bf162e10d415fe56084424c59
Akovi Mensah <akovi66@gmail.com>
2022-07-29 12:30:03 -0400

    add and remove item in basket

--- API/DTOs/BasketItemDto.cs
+++ API/DTOs/BasketItemDto.cs
@@ -6,7 +6,7 @@ namespace API.DTOs
         public string Name { get; set; }
         public long Price { get; set; }
         public string PictureUrl { get; set; }
-        public string Brand { get; set; }
+        public string Crust { get; set; }
         public string Type { get; set; }
         public int Quantity { get; set; }
     }
--- API/Entities/Basket.cs
+++ API/Entities/Basket.cs
@@ -9,20 +9,20 @@ namespace API.Entities
         public string BuyerId { get; set; }
         public List<BasketItem> Items { get; set; } = new();
 
-        public void AddItem(Pizza Pizza, int quantity)
+        public void AddItem(Pizza pizza, int quantity)
         {
-            if (Items.All(item => item.PizzaId != Pizza.Id))
+            if (Items.All(item => item.PizzaId != pizza.Id))
             {
-                Items.Add(new BasketItem{Pizza = Pizza, Quantity = quantity});
+                Items.Add(new BasketItem{Pizza = pizza, Quantity = quantity});
             }
 
-            var existingItem = Items.FirstOrDefault(item => item.PizzaId == Pizza.Id);
+            var existingItem = Items.FirstOrDefault(item => item.PizzaId == pizza.Id);
             if (existingItem != null) existingItem.Quantity += quantity;
         }
 
-        public void RemoveItem(int PizzaId, int quantity)
+        public void RemoveItem(int pizzaId, int quantity)
         {
-            var item = Items.FirstOrDefault(item => item.PizzaId == PizzaId);
+            var item = Items.FirstOrDefault(item => item.PizzaId == pizzaId);
             if (item == null) return;
             item.Quantity -= quantity;
             if (item.Quantity == 0) Items.Remove(item);
